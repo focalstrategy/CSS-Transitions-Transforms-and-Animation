80144826fc2f4f02884017ef681986d56fa23650
Rich Bradshaw <rich.bradshaw@gmail.com>
2010-05-23 08:48:12 +0100

    Transforms are accelerated in iPhone OS

--- slide2.php
+++ slide2.php
@@ -1,5 +1,5 @@
 <h2 id="slide2">Demo 2 - Sliding by translating the images (transitions and transforms)</h2>
-<p class="note">Note: Despite apparently being hardware accelerated, this seems very choppy on iPhone OS.</p>
+<p class="note">Note: Animating by transitioning transforms is hardware accelerate on iPhone OS, making this preferable there.</p>
 <h3>Plan</h3>
 <ol>
 	<li>Create a container with overflow set to hidden.</li>
