1de01e982d19113f75485ce879712840d5d82045
Rich Bradshaw <rich.bradshaw@gmail.com>
2012-02-12 17:44:13 +0000

    Added disqus comments

--- index.php
+++ index.php
@@ -53,6 +53,21 @@ $section = 	$_GET['section'];
 	src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
 	</script>	
 	<?include($_SERVER['DOCUMENT_ROOT']."/".$section.".php");?>
+
+	<div id="disqus_thread"></div>
+	<script type="text/javascript">
+	    var disqus_shortname = 'css3bradshaw';
+	    var disqus_identifier = '<?= $section ?>';
+	    (function() {
+	        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+	        dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+	        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+	    })();
+	</script>
+	<noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+	<a href="http://disqus.com" class="dsq-brlink">blog comments powered by <span class="logo-disqus">Disqus</span></a>
+
+
 </section>
 
 <section id="inpage-contents">
