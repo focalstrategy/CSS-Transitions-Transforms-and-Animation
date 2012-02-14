bf79011553bf6257bf996a06bb9aa7b9930ef2e7
Rich Bradshaw <rich.bradshaw@gmail.com>
2012-02-14 14:37:50 +0000

    Updated info about hardware acceleration

--- references.php
+++ references.php
@@ -7,7 +7,6 @@
 		<li><a href="http://www.w3.org/TR/css3-3d-transforms/">W3C: CSS 3D Transforms Module Level 3</a></li>
 		<li><a href="http://www.w3.org/TR/css3-animations/">W3C: CSS Animations Module Level 3</a></li>												
 		<li><a href="http://webkit.org/blog/">Surfin' Safari (check the archives)</a></li>
-		<li><a href="http://www.opera.com/docs/specs/presto23/css/transitions/">CSS3 Transitions support in Opera Presto 2.3</a></li>
 		<li><a href="http://desandro.github.com/3dtransforms/">David Desandro's 3D transform examples</a></li>
 	</ul>
 </section>
